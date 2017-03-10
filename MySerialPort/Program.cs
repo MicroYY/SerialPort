5bfd98edfcfa0b30b82f7e823d1fb48d6e609d46
unknown <>
2017-03-10 11:07:01 +0800

    添加项目文件。

---
+++ MySerialPort/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO.Ports;
+
+namespace MySerialPort
+{
+    class Program
+    {
+        static bool _continue;
+        static SerialPort _myserialPort;
+
+        static void Main(string[] args)
+        {
+            Thread readThread = new Thread(Read);
+            _myserialPort = new SerialPort("COM3", 115200, 0, 8, StopBits.One);
+
+            _myserialPort.ReadTimeout = 500;
+            _myserialPort.WriteTimeout = 500;
+
+            _myserialPort.Open();
+            _continue = true;
+            readThread.Start();
+
+        }
+
+        public static void Read()
+        {
+            while (_continue)
+            {
+                try
+                {
+                    string message = _myserialPort.ReadLine();
+                    Console.WriteLine(message);
+                }
+                catch (TimeoutException) { }
+            }
+        }
+    }
+}
